bd113b995dcdabb4503aaf8f8ab8af0c7d1cc0dd
wisky026 <137921157+wisky026@users.noreply.github.com>
2024-01-10 21:18:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const noBtn = document.querySelector('.no-btn');
 const wrapperRect = wrapper.getBoundingClientRect();
 const noBtnRect = noBtn.getBoundingClientRect();
 yesBtn.addEventListener('click', () => {
-    question.innerHTML = 'I Love You Too :)';
+    question.innerHTML = 'Ao karain baat qt :)';
 });
 noBtn.addEventListener('mouseover', () => {
     const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
